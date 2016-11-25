c1d4154ef689c61efff1b9af9628755d953a45d6
FaisalAli19 <Faisalali1901@gmail.com>
2016-11-25 14:41:27 +0530

    fixed scrolling issue

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -44,7 +44,7 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	"use strict";
+	'use strict';
 
 	var _MobileMenu = __webpack_require__(1);
 
@@ -62,17 +62,12 @@
 
 	var _StickyHeader2 = _interopRequireDefault(_StickyHeader);
 
-	var _Modal = __webpack_require__(7);
-
-	var _Modal2 = _interopRequireDefault(_Modal);
-
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 	var mobileMenu = new _MobileMenu2.default();
 	new _RevealOnScroll2.default((0, _jquery2.default)(".feature-item"), "85%");
 	new _RevealOnScroll2.default((0, _jquery2.default)(".testimonial"), "60%");
 	var stickyHeader = new _StickyHeader2.default();
-	var modal = new _Modal2.default();
 
 /***/ },
 /* 1 */
@@ -10353,10 +10348,10 @@
 /* 3 */
 /***/ function(module, exports, __webpack_require__) {
 
-	"use strict";
+	'use strict';
 
 	Object.defineProperty(exports, "__esModule", {
-	    value: true
+	  value: true
 	});
 
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -10374,38 +10369,38 @@
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 	var RevealOnScroll = function () {
-	    function RevealOnScroll(els, offset) {
-	        _classCallCheck(this, RevealOnScroll);
+	  function RevealOnScroll(els, offset) {
+	    _classCallCheck(this, RevealOnScroll);
 
-	        this.itemsToReveal = els;
-	        this.offsetPercent = offset;
-	        this.hideInitially();
-	        this.createWaypoints();
+	    this.itemsToReveal = els;
+	    this.offsetPercentage = offset;
+	    this.hideInitially();
+	    this.createWaypoints();
+	  }
+
+	  _createClass(RevealOnScroll, [{
+	    key: 'hideInitially',
+	    value: function hideInitially() {
+	      this.itemsToReveal.addClass("reveal-item");
 	    }
+	  }, {
+	    key: 'createWaypoints',
+	    value: function createWaypoints() {
+	      var that = this;
+	      this.itemsToReveal.each(function () {
+	        var currentItem = this;
+	        new Waypoint({
+	          element: currentItem,
+	          handler: function handler() {
+	            (0, _jquery2.default)(currentItem).addClass("reveal-item--is-visible");
+	          },
+	          offset: that.offsetPercentage
+	        });
+	      });
+	    }
+	  }]);
 
-	    _createClass(RevealOnScroll, [{
-	        key: "hideInitially",
-	        value: function hideInitially() {
-	            this.itemsToReveal.addClass("reveal-item");
-	        }
-	    }, {
-	        key: "createWaypoints",
-	        value: function createWaypoints() {
-	            var that = this;
-	            this.itemsToReveal.each(function () {
-	                var currentItem = this;
-	                new Waypoint({
-	                    element: currentItem,
-	                    handler: function handler() {
-	                        (0, _jquery2.default)(currentItem).addClass("reveal-item--is-visible");
-	                    },
-	                    offset: that.offsetPercent
-	                });
-	            });
-	        }
-	    }]);
-
-	    return RevealOnScroll;
+	  return RevealOnScroll;
 	}();
 
 	exports.default = RevealOnScroll;
@@ -11605,72 +11600,6 @@
 	}));
 
 
-
-/***/ },
-/* 7 */
-/***/ function(module, exports, __webpack_require__) {
-
-	"use strict";
-
-	Object.defineProperty(exports, "__esModule", {
-	    value: true
-	});
-
-	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-	var _jquery = __webpack_require__(2);
-
-	var _jquery2 = _interopRequireDefault(_jquery);
-
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-	var Modal = function () {
-	    function Modal() {
-	        _classCallCheck(this, Modal);
-
-	        this.openModalButton = (0, _jquery2.default)(".open-modal");
-	        this.modal = (0, _jquery2.default)(".modal");
-	        this.modelClose = (0, _jquery2.default)(".modal__close");
-
-	        this.events();
-	    }
-
-	    _createClass(Modal, [{
-	        key: "events",
-	        value: function events() {
-	            //Clicking the button
-	            this.openModalButton.click(this.openModal.bind(this));
-	            //clicking close
-	            this.modelClose.click(this.closeModal.bind(this));
-	            //pressing any key
-	            (0, _jquery2.default)(document).keyup(this.keyPressHandler.bind(this));
-	        }
-	    }, {
-	        key: "keyPressHandler",
-	        value: function keyPressHandler(e) {
-	            if (e.keyCode == 27) {
-	                this.closeModal();
-	            }
-	        }
-	    }, {
-	        key: "openModal",
-	        value: function openModal() {
-	            this.modal.addClass("modal--is-visible");
-	            return false;
-	        }
-	    }, {
-	        key: "closeModal",
-	        value: function closeModal() {
-	            this.modal.removeClass("modal--is-visible");
-	        }
-	    }]);
-
-	    return Modal;
-	}();
-
-	exports.default = Modal;
 
 /***/ }
 /******/ ]);
